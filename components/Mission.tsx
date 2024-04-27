082ea46a68828171597866449f9f5d426d10da3f
Afaq Ahmed <khatri.afaq@gmail.com>
2024-04-27 19:39:28 +0500

    vision and mission

--- components/Mission.tsx
+++ components/Mission.tsx
@@ -11,7 +11,7 @@ const AboutUsSection = () => {
 
         <div className="flex flex-col p-5 order-2 md:pl-24">
           <div className="flex md:mt-28">
-            <h1 className="m-0 relative text-inherit leading-[72px] font-bold font-poppins inline-block [filter:drop-shadow(0px_4px_14px_rgba(0,_0,_0,_0.5))] max-w-full z-[3] sm:text-17xl sm:leading-[43px] lg:text-[48px] lg:leading-[58px]">
+            <h1 className="m-0 relative text-inherit leading-[72px] font-bold font-poppins inline-block [filter:drop-shadow(0px_4px_14px_rgba(0,_0,_0,_0.5))] max-w-full z-[3] sm:text-15xl sm:leading-[43px] lg:text-[48px] lg:leading-[58px]">
               <span className="text-transparent !bg-clip-text [background:linear-gradient(180deg,_#fff,_rgba(255,_255,_255,_0))] [-webkit-background-clip:text] [-webkit-text-fill-color:transparent]">
                 OUR
               </span>
@@ -20,21 +20,9 @@ const AboutUsSection = () => {
             </h1>
           </div>
 
-          <div className="relative text-xl leading-[150%] font-poppins text-lightsteelblue z-[1] mq450:text-base mq450:leading-[24px]">
-            <p className="m-0">
-              Our mission is to develop a vibrant community of lifelong
-            </p>
-            <p className="m-0">
-              learners through accessible, inclusive, and innovative
-            </p>
-            <p className="m-0">
-              education. Guided by collaborative engagement and
-            </p>
-            <p className="m-0">
-              rooted in biblical values, we empower students to excel
-            </p>
-            <p className="m-0">
-              spiritually, academically, socially, and personally.
+          <div className=" max-w-lg relative text-xl leading-[150%] font-poppins text-lightsteelblue z-[1] mq450:text-base mq450:leading-[24px]">
+            <p className="m-0 leading-9">
+              Our mission is to develop a vibrant community of lifelong learners through accessible, inclusive, and innovative education. Guided by collaborative engagement and rooted in biblical values, we empower students to excel spiritually, academically, socially, and personally.
             </p>
           </div>
         </div>
